11c925360953e569e9eadd383653a9282dfa8f9a
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-04-13 10:17:50 +0530

    Update reload.py

--- YukkiMusic/plugins/tools/reload.py
+++ YukkiMusic/plugins/tools/reload.py
@@ -9,16 +9,16 @@
 import asyncio
 
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import CallbackQuery, Message
 
-from config import BANNED_USERS, MUSIC_BOT_NAME, adminlist
+from config import BANNED_USERS, MUSIC_BOT_NAME, adminlist, lyrical
 from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.misc import db
-from YukkiMusic.utils.database import get_authuser_names
-from YukkiMusic.utils.decorators import language
-from YukkiMusic.utils.decorators.admins import AdminActual
+from YukkiMusic.utils.database import get_authuser_names, get_cmode
+from YukkiMusic.utils.decorators import (ActualAdminCB, AdminActual,
+                                         language)
 from YukkiMusic.utils.formatters import alpha_to_int
 
 ### Multi-Lang Commands
@@ -71,6 +71,17 @@ async def restartbot(client, message: Message, _):
         await Yukki.stop_stream(message.chat.id)
     except:
         pass
+    chat_id = await get_cmode(message.chat.id)
+    if chat_id:
+        try:
+            await app.get_chat(chat_id)
+        except:
+            pass
+        try:
+            db[chat_id] = []
+            await Yukki.stop_stream(chat_id)
+        except:
+            pass
     return await mystic.edit_text(
         "Successfully restarted. Try playing now.."
     )
@@ -83,3 +94,50 @@ async def close_menu(_, CallbackQuery):
         await CallbackQuery.answer()
     except:
         return
+
+
+@app.on_callback_query(filters.regex("close") & ~BANNED_USERS)
+async def close_menu(_, CallbackQuery):
+    try:
+        await CallbackQuery.message.delete()
+        await CallbackQuery.answer()
+    except:
+        return
+
+
+@app.on_callback_query(
+    filters.regex("stop_downloading") & ~BANNED_USERS
+)
+@ActualAdminCB
+async def stop_download(client, CallbackQuery: CallbackQuery, _):
+    message_id = CallbackQuery.message.message_id
+    task = lyrical.get(message_id)
+    if not task:
+        return await CallbackQuery.answer(
+            "Downloading already Completed.", show_alert=True
+        )
+    if task.done() or task.cancelled():
+        return await CallbackQuery.answer(
+            "Downloading already Completed or Cancelled.",
+            show_alert=True,
+        )
+    if not task.done():
+        try:
+            task.cancel()
+            try:
+                lyrical.pop(message_id)
+            except:
+                pass
+            await CallbackQuery.answer(
+                "Downloading Cancelled", show_alert=True
+            )
+            return await CallbackQuery.edit_message_text(
+                f"Download Cancelled by {CallbackQuery.from_user.mention}"
+            )
+        except:
+            return await CallbackQuery.answer(
+                "Failed to stop the Downloading.", show_alert=True
+            )
+    await CallbackQuery.answer(
+        "Failed to recognize the running task", show_alert=True
+    )
